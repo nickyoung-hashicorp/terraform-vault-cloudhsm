ff77f68960499f6f4dff7b4014757740b4490e16
nickyoung-hashicorp <nyoung@hashicorp.com>
2021-07-21 11:18:03 -0700

    updated .tf files

--- run-vault-hsm.sh
+++ run-vault-hsm.sh
@@ -2,6 +2,8 @@ echo "Starting Vault systemd service"
 sudo systemctl enable vault
 sudo systemctl start vault
 
+export VAULT_ADDR=http://127.0.0.1:8200
+
 sleep 5
 echo "Initializing Vault with vault operator init..."
 vault operator init -key-shares=1 -key-threshold=1 -recovery-shares=1 -recovery-threshold=1 > vault_init.json
